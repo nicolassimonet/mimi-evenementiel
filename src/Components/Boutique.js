b5c632380e3e2a30ed13397ddbbca06882efc68e
nicolassimonet <75303330+nicolassimonet@users.noreply.github.com>
2023-02-22 17:14:08 +0100

    ajout bouton location + videoBg

--- src/Components/Boutique.js
+++ src/Components/Boutique.js
@@ -12,6 +12,7 @@ class Portfolio extends Component {
           <div className="boutique__container">
             <h1 className="blurb__heading text__boutique section-heading">boutique</h1>
                 <a className="link__button-text" href="https://lescadeauxdemimi.fr/products/" target="_blank" rel="noopener noreferrer">Visitez la boutique</a>
+                <a className="link__button-text" href="https://lescadeauxdemimi.fr/products/" target="_blank" rel="noopener noreferrer">Vous souhaitez louer ?</a>
           </div>
         </section>
       </Fade>
